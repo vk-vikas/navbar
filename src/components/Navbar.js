ddeeae6c279269f8f2baac65d9806248b6c0b20d
vikas kumar <vk.vikas3003@gmail.com>
2021-12-18 17:43:40 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+        <div>
+            <h2>website name</h2>
+            
+        </div>
+        <div>
+            <ul>
+                <li>components</li>
+                <li>help</li>
+                <li>contact us</li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
