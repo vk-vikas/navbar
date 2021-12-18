2bcb01d8e53526b749f500851001585cd406c308
vikas kumar <vk.vikas3003@gmail.com>
2021-12-18 18:24:56 +0530

    css added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import './Navbar.css'
 const Navbar = () => {
     return (
         <>
-        <div>
-            <h2>website name</h2>
-            
-        </div>
-        <div>
+        <div className='navbar-container'>
+        
+        <h2>website name</h2>
+    
             <ul>
                 <li>components</li>
                 <li>help</li>
                 <li>contact us</li>
             </ul>
+    
         </div>
+        
         </>
     )
 }
